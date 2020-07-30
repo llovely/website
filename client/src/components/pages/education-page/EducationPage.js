4fc8912d75f98d9bf1dee087ef9824ef821f50a1
Luis Love <luisl.love25@gmail.com>
2020-07-30 01:19:14 -0500

    Added education and skill information to the education page.

--- client/src/components/pages/education-page/EducationPage.js
+++ client/src/components/pages/education-page/EducationPage.js
@@ -2,6 +2,11 @@ import React from 'react';
 import PageContainer from '../PageContainer';
 import { PAGE_WORK_ID } from '../../constants/Constants';
 
+import { ReactComponent as DegreeIcon } from '../../../icons/degree.svg';
+import { ReactComponent as GraduationIcon } from '../../../icons/education.svg';
+import { ReactComponent as LanguagesIcon } from '../../../icons/coding.svg';
+import { ReactComponent as SkillsIcon } from '../../../icons/skills-2.svg';
+
 export default function EducationPage(props) {
   return (
     <div id={props.id}>
@@ -23,6 +28,56 @@ export default function EducationPage(props) {
                                         University of Illinois at Chicago
                                    </a> (UIC).
             </p>
+          </div>
+
+          <div className='page-row'>
+            <div className='ep-item-container'>
+              <div className='ep-item-icon'>
+                <DegreeIcon/>
+              </div>
+              <div className='ep-item-header'>
+                Degree
+              </div>
+              <div className='ep-item-message'>
+                Bachelor of Science in Computer Science, <span style={{'font-style': 'italic'}}>summa cum laude</span>
+              </div>
+            </div>
+
+            <div className='ep-item-container'>
+              <div className='ep-item-icon'>
+                <GraduationIcon/>
+              </div>
+              <div className='ep-item-header'>
+                Graduation
+              </div>
+              <div className='ep-item-message'>
+                December 2019
+              </div>
+            </div>
+
+            <div className='ep-item-container'>
+              <div className='ep-item-icon'>
+                <LanguagesIcon/>
+              </div>
+              <div className='ep-item-header'>
+                Languages
+              </div>
+              <div className='ep-item-message'>
+                Python, Scala, Java, C/C++, OCaml, SQL, Dart, JavaScript
+              </div>
+            </div>
+
+            <div className='ep-item-container'>
+              <div className='ep-item-icon'>
+                <SkillsIcon/>
+              </div>
+              <div className='ep-item-header'>
+                Skills
+              </div>
+              <div className='ep-item-message'>
+                Git, x86 Assembly, MySQL, Bash Scripting, Docker, Flutter, React
+              </div>
+            </div>
           </div>
 
           <div className='page-row'>
