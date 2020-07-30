ccafce0ad7571a51a3bc659f10192a24a0a05c88
Luis Love <luisl.love25@gmail.com>
2020-07-30 01:22:08 -0500

    Added work experience to the work page.

--- client/src/components/pages/work-page/WorkPage.js
+++ client/src/components/pages/work-page/WorkPage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PageContainer from '../PageContainer';
 import { PAGE_PROJECTS_ID } from '../../constants/Constants';
 
+import { ReactComponent as VmwareIcon } from '../../../icons/vmware.svg';
+import { ReactComponent as UICIcon } from '../../../icons/uic.svg';
+
 export default function WorkPage(props) {
   return (
     <div id={props.id}>
@@ -18,6 +21,43 @@ export default function WorkPage(props) {
             <p className='page-message wp-message'>
               I'm a recent graduate seeking full-time employment as a SWE.
             </p>
+          </div>
+
+          <div className='page-row'>
+            <div className='wp-item-container'>
+              <div className='wp-item-icon'>
+                <VmwareIcon/>
+              </div>
+              <div className='wp-item-header'>
+                Intern - Performance
+              </div>
+              <a href='https://www.vmware.com/'
+                 target='_blank'
+                 rel='noopener noreferrer'
+                 className='wp-employer-name'>
+                  VMware
+              </a>
+              <div className='wp-item-message'>
+                May 2018 - August 2018
+              </div>
+            </div>
+
+            <div className='wp-item-container'>
+              <div className='wp-item-icon'>
+                <UICIcon/>
+              </div>
+              <div className='wp-item-header'>
+                Undergradute Researcher
+              </div>
+              <a href='https://bits.lab.uic.edu/'
+                 target='_blank'
+                 rel='noopener noreferrer'
+                 className='wp-employer-name'>BITS Networked Systems Lab at UIC
+              </a>
+              <div className='wp-item-message'>
+                May 2019 - December 2019
+              </div>
+            </div>
           </div>
 
           <div className='page-row'>
