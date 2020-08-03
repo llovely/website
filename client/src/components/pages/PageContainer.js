6801a7118ac338be5fd0840625e6cadc6cbb6f76
Luis Love <luisl.love25@gmail.com>
2020-08-03 15:10:23 -0500

    Refactored code.

--- client/src/components/pages/PageContainer.js
+++ client/src/components/pages/PageContainer.js
@@ -1,11 +1,30 @@
+/*
+ * PageContainer.js
+ *
+ * Component is intended to hold the contents of a page (section) of
+ * the website.
+ *
+ * Author: Luis Love
+*/
 
 import React from 'react';
+import './pages.css';
+
 
 export default function PageContainer(props) {
+
+  // Additional css properties to apply
+  const moreStyles = {
+    'background-image': `url(${props.backgroundImage})`,
+    'background-color': `${props.backgroundColor}`
+  }
+  
   return (
-    <div className={props.pageStyle} style={props.style}>
-      <div className={props.filter}>
-        {props.children}
+    <div className={`page-background ${props.mediaClass}`} style={moreStyles}>
+      <div className={props.backgroundFilter}>
+        <div className='page-content-container'>
+          {props.children}
+        </div>
       </div>
     </div>
   );
